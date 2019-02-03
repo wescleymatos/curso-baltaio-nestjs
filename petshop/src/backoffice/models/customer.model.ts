5d2f7c7d51e5287c94006cbc895ddc575e64cc43
Wescley Matos <wescleymatos@gmail.com>
2019-02-03 20:47:43 -0300

    Criação do schema de usuário

--- petshop/src/backoffice/models/customer.model.ts
+++ petshop/src/backoffice/models/customer.model.ts
@@ -1,6 +1,7 @@
 import { Pet } from './pet.model';
 import { Address } from './address.model';
 import { CreditCard } from './credit-card.model';
+import { User } from './user.model';
 
 export class Customer {
     constructor(
@@ -11,8 +12,7 @@ export class Customer {
         public billingAddress: Address,
         public shippingAddress: Address,
         public creditCard: CreditCard,
-        public password: string,
-        public active: boolean,
+        public user: User,
     ) {
     }
 }
